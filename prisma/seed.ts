36dc0948b58e4ad7bf171523161d8b457b583d89
cauecermakdev <caue.cermak@usp.br>
2023-03-09 12:15:15 -0300

    seed ticketType

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from "@prisma/client";
-import dayjs from "dayjs";
+import { PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
+import { arrayBuffer } from 'stream/consumers';
 const prisma = new PrismaClient();
 
 async function main() {
@@ -7,14 +8,44 @@ async function main() {
   if (!event) {
     event = await prisma.event.create({
       data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+        title: 'Driven.t',
+        logoImageUrl: 'https://files.driveneducation.com.br/images/logo-rounded.png',
+        backgroundImageUrl: 'linear-gradient(to right, #FA4098, #FFD77F)',
         startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
+        endsAt: dayjs().add(21, 'days').toDate(),
       },
     });
   }
+
+  async function deleteOnCascadeAllTicketTypes() {
+    await prisma.ticket.deleteMany({});
+    await prisma.ticketType.deleteMany({});
+    const ticketTypeArray = await prisma.ticketType.createMany({
+      data: [
+        {
+          name: 'online',
+          price: 150,
+          isRemote: true,
+          includesHotel: false,
+        },
+        {
+          name: 'presencial sem hotel',
+          price: 250,
+          isRemote: false,
+          includesHotel: true,
+        },
+        {
+          name: 'presencial com hotel',
+          price: 450,
+          isRemote: false,
+          includesHotel: true,
+        },
+      ],
+    });
+    console.log({ ticketTypeArray });
+  }
+
+  deleteOnCascadeAllTicketTypes();
 
   console.log({ event });
 }
