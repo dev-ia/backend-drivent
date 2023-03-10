625957bc19da7394c2ffd0aa59bc8ff9cac05aaf
cauecermakdev <caue.cermak@usp.br>
2023-03-09 23:04:04 -0300

    version scratch from full spring 1

--- src/repositories/ticket-repository/index.ts
+++ src/repositories/ticket-repository/index.ts
@@ -1,5 +1,5 @@
-import { prisma } from "@/config";
-import { Ticket, TicketStatus, TicketType } from "@prisma/client";
+import { prisma } from '@/config';
+import { Ticket, TicketStatus, TicketType } from '@prisma/client';
 
 async function findTicketTypes() {
   return prisma.ticketType.findMany();
@@ -12,7 +12,7 @@ async function findTickeyById(ticketId: number) {
     },
     include: {
       Enrollment: true,
-    }
+    },
   });
 }
 async function findTickeWithTypeById(ticketId: number) {
@@ -22,7 +22,7 @@ async function findTickeWithTypeById(ticketId: number) {
     },
     include: {
       TicketType: true,
-    }
+    },
   });
 }
 
@@ -33,7 +33,18 @@ async function findTicketByEnrollmentId(enrollmentId: number) {
     },
     include: {
       TicketType: true, //inner join
-    }
+    },
+  });
+}
+
+async function findTicketByTicketId(ticketId: number) {
+  return prisma.ticket.findFirst({
+    where: {
+      id: ticketId,
+    },
+    include: {
+      TicketType: true, //inner join
+    },
   });
 }
 
@@ -41,7 +52,7 @@ async function createTicket(ticket: CreateTicketParams) {
   return prisma.ticket.create({
     data: {
       ...ticket,
-    }
+    },
   });
 }
 
@@ -49,7 +60,7 @@ async function createTicketType(ticketType: CreateTicketTypeParams) {
   return prisma.ticketType.create({
     data: {
       ...ticketType,
-    }
+    },
   });
 }
 
@@ -60,12 +71,12 @@ async function ticketProcessPayment(ticketId: number) {
     },
     data: {
       status: TicketStatus.PAID,
-    }
+    },
   });
 }
 
-export type CreateTicketParams = Omit<Ticket, "id" | "createdAt" | "updatedAt">
-export type CreateTicketTypeParams = Omit<TicketType, "id" | "createdAt" | "updatedAt">
+export type CreateTicketParams = Omit<Ticket, 'id' | 'createdAt' | 'updatedAt'>;
+export type CreateTicketTypeParams = Omit<TicketType, 'id' | 'createdAt' | 'updatedAt'>;
 
 const ticketRepository = {
   findTicketTypes,
@@ -75,6 +86,7 @@ const ticketRepository = {
   findTickeyById,
   findTickeWithTypeById,
   ticketProcessPayment,
+  findTicketByTicketId,
 };
 
 export default ticketRepository;
